7b8aab51ae798ff0e0940c45d380e8bd0e0d86a3
nguyenvanduc198 <nguyenmanh.vanduc@gmail.com>
2021-11-30 06:57:06 +0900

    hellolanguege

---
+++ konichiwa.php
@@ -0,0 +1,37 @@
+<?php
+
+use JetBrains\PhpStorm\Internal\LanguageLevelTypeAware;
+use JetBrains\PhpStorm\Language;
+
+$userListInput = [
+    [
+        "username" => "Karl",
+        "language" => "en"
+    ], [
+        "username" => "Tam",
+        "language" => "vi"
+    ], [
+        "username" => "Kazuki",
+        "language" => "ja"
+    ]
+    
+];
+function getNameMessage ($username,$userListInput){
+$message = '';
+foreach ($userListInput as $user){
+if($username === $user['username']){
+    if($user['language'] ==='vi'){
+        $message = 'xin chao'.$username;
+    }elseif($user['language'] ==='en'){
+        $message = 'hello'.$username;
+    }elseif($user['language'] ==='ja'){
+        $message = 'konichiwa'.$username;
+    }
+}
+}
+return $message;
+}
+echo getNameMessage('Tam', $userListInput)
+
+
+?>
